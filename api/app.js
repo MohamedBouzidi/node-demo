76963d32f6928da5c205f54c51cd438b3b6e9681
EC2 Default User <ec2-user@ip-10-10-1-101.ec2.internal>
2021-05-25 06:47:19 +0000

    Update database logic

--- api/app.js
+++ api/app.js
@@ -1,101 +1,9 @@
-require('dotenv').config();
-const uuid = require('uuid');
-const redis = require('redis');
-const mysql = require('mysql');
 const morgan = require('morgan');
 const express = require('express');
-const { promisify } = require('util');
 const path = require('path');
 const winston = require('winston');
 const cors = require('cors');
-
-const client = redis.createClient({
-    host: process.env.REDIS_URL,
-    port: process.env.REDIS_PORT,
-});
-const conn = mysql.createConnection({
-    host: process.env.DATABASE_URL,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASS,
-    database: process.env.DATABASE_NAME,
-});
-
-client.on('error', console.error);
-
-const redisSetAsync = promisify(client.set).bind(client);
-const redisGetAsync = promisify(client.get).bind(client);
-const mysqlConnectAsync = promisify(conn.connect).bind(conn);
-const mysqlQueryAsync = promisify(conn.query).bind(conn);
-
-const createTable = async () => {
-    try {
-        await mysqlQueryAsync(
-            'CREATE TABLE people (id VARCHAR(255) NOT NULL PRIMARY KEY, name VARCHAR(255), age INTEGER)'
-        );
-    } catch (e) {
-        console.log('table exists already!');
-    }
-};
-
-const dropTable = async () => {
-    try {
-        await mysqlQueryAsync('DROP TABLE IF EXISTS people');
-    } catch (e) {
-        console.log('table does not exist!');
-    }
-};
-
-const insertPerson = async (name, age) => {
-    try {
-        return await mysqlQueryAsync(
-            `INSERT INTO people (id, name, age) VALUES ('${uuid.v4()}', '${name}', ${age})`
-        );
-    } catch (e) {
-        return e;
-    }
-};
-
-const selectAllPeople = async () => {
-    try {
-        return await mysqlQueryAsync('SELECT * FROM people');
-    } catch (e) {
-        return e;
-    }
-};
-
-const selectPerson = async (id, logger = { info: console.log }) => {
-    let person = await redisGetAsync(id);
-    logger.info('from cache:', person);
-    if (person) return JSON.parse(person);
-    try {
-        person = await mysqlQueryAsync(
-            `SELECT * FROM people WHERE id = '${id}'`
-        );
-        await redisSetAsync(id, JSON.stringify(person));
-        logger.info('from db:', person);
-        return person;
-    } catch (e) {
-        return e;
-    }
-};
-
-const updatePerson = async (id, name, age) => {
-    try {
-        return await mysqlQueryAsync(
-            `UPDATE people SET name = ${name}, age = ${age} WHERE id = ${id}`
-        );
-    } catch (e) {
-        return e;
-    }
-};
-
-const deletePerson = async id => {
-    try {
-        return await mysqlQueryAsync(`DELETE FROM people WHERE id = ${id}`);
-    } catch (e) {
-        return e;
-    }
-};
+const db = require('./db');
 
 const initLogger = () => {
     const options = {
@@ -135,8 +43,7 @@ const initLogger = () => {
 };
 
 const runApplication = async () => {
-    await mysqlConnectAsync();
-    await createTable();
+    await db.openConnection();
 
     const app = express();
     const port = process.env.APP_PORT || 3000;
@@ -162,22 +69,22 @@ const runApplication = async () => {
     });
 
     app.get('/people', async (req, res) => {
-        const people = await selectAllPeople();
+        const people = await db.selectAllPeople();
         res.json(people);
     });
 
     app.get('/person/:id', async (req, res) => {
-        const person = await selectPerson(req.params.id, logger);
+        const person = await db.selectPerson(req.params.id, logger);
         res.json(person);
     });
 
     app.post('/person', async (req, res) => {
-        const person = await insertPerson(req.body.name, req.body.age);
+        const person = await db.insertPerson(req.body.name, req.body.age);
         res.json(person);
     });
 
     app.put('/person/:id', async (req, res) => {
-        const person = await updatePerson(
+        const person = await db.updatePerson(
             req.params.id,
             req.body.name,
             req.body.age
@@ -186,14 +93,13 @@ const runApplication = async () => {
     });
 
     app.delete('/person/:id', async (req, res) => {
-        await deletePerson(req.params.id);
+        await db.deletePerson(req.params.id);
         res.end();
     });
 
     process.on('SIGINT', () => {
         console.log('closing app');
-        client.end(true);
-        conn.destroy();
+        db.closeConnection();
         process.exit();
     });
 };
---
+++ api/db.js
@@ -0,0 +1,131 @@
+require('dotenv').config();
+const uuid = require('uuid');
+const redis = require('redis');
+const mysql = require('mysql');
+const { promisify } = require('util');
+
+let conn = null;
+let client = null;
+
+let redisSetAsync = null;
+let redisGetAsync = null;
+let mysqlQueryAsync = null;
+
+const openConnection = async () => {
+    if (!client) {
+        client = redis.createClient({
+            host: process.env.REDIS_URL,
+            port: process.env.REDIS_PORT,
+        });
+        client.on('error', console.error);
+        redisSetAsync = promisify(client.set).bind(client);
+        redisGetAsync = promisify(client.get).bind(client);
+    }
+    
+    if (!conn) {
+        conn = mysql.createConnection({
+            host: process.env.DATABASE_URL,
+            user: process.env.DATABASE_USER,
+            password: process.env.DATABASE_PASS,
+            database: process.env.DATABASE_NAME,
+        });
+        mysqlQueryAsync = promisify(conn.query).bind(conn);
+        return await promisify(conn.connect).bind(conn)();
+    }
+};
+
+const closeConnection = () => {
+    if (!client) client.end(true);
+    if (!conn) conn.destroy();
+}
+
+const createTable = async () => {
+    try {
+        await mysqlQueryAsync(
+            'CREATE TABLE people (id VARCHAR(255) NOT NULL PRIMARY KEY, name VARCHAR(255), phone VARCHAR(255))'
+        );
+    } catch (e) {
+        console.log('table exists already!');
+    }
+};
+
+const dropTable = async () => {
+    try {
+        await mysqlQueryAsync('DROP TABLE IF EXISTS people');
+    } catch (e) {
+        console.log('table does not exist!');
+    }
+};
+
+const insertPerson = async (name, phone) => {
+    try {
+        return await mysqlQueryAsync(
+            `INSERT INTO people (id, name, phone) VALUES ('${uuid.v4()}', '${name}', ${phone})`
+        );
+    } catch (e) {
+        return e;
+    }
+};
+
+const insertPeople = async people => {
+    try {
+        const values = people.map(p => `('${uuid.v4()}', '${p.name}', '${p.phone}')`).join(', ');
+        return await mysqlQueryAsync('INSERT INTO people (id, name, phone) VALUES ' + values);
+    } catch (e) {
+        return e;
+    }
+};
+
+const selectAllPeople = async () => {
+    try {
+        return await mysqlQueryAsync('SELECT * FROM people');
+    } catch (e) {
+        return e;
+    }
+};
+
+const selectPerson = async (id, logger = { info: console.log }) => {
+    let person = await redisGetAsync(id);
+    logger.info('from cache:', person);
+    if (person) return JSON.parse(person);
+    try {
+        person = await mysqlQueryAsync(
+            `SELECT * FROM people WHERE id = '${id}'`
+        );
+        await redisSetAsync(id, JSON.stringify(person));
+        logger.info('from db:', person);
+        return person;
+    } catch (e) {
+        return e;
+    }
+};
+
+const updatePerson = async (id, name, phone) => {
+    try {
+        return await mysqlQueryAsync(
+            `UPDATE people SET name = ${name}, phone = ${phone} WHERE id = ${id}`
+        );
+    } catch (e) {
+        return e;
+    }
+};
+
+const deletePerson = async id => {
+    try {
+        return await mysqlQueryAsync(`DELETE FROM people WHERE id = ${id}`);
+    } catch (e) {
+        return e;
+    }
+};
+
+module.exports = {
+    openConnection,
+    closeConnection,
+    createTable,
+    selectPerson,
+    selectAllPeople,
+    insertPerson,
+    insertPeople,
+    updatePerson,
+    deletePerson
+};
